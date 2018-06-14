747b7e37410d546a17d8ca31e9793160d7809bb6
Andrea Aime <andrea.aime@gmail.com>
2018-06-14 14:38:09 +0200

    Forgot reference files for RuntimeModelTest

---
+++ language/src/test/resources/reference/mandelbrot-DIRECT.java
@@ -0,0 +1,50 @@
+package org.jaitools.jiffle.runtime;
+
+public class JiffleDirectRuntimeImpl extends org.jaitools.jiffle.runtime.AbstractDirectRuntime {
+    boolean _imageScopeVarsInitialized = false;
+    double v_MaxIter;
+    double v_MinRe;
+    double v_MaxRe;
+    double v_MinIm;
+    double v_MaxIm;
+    double v_Re_scale;
+    double v_Im_scale;
+
+    protected void initImageScopeVars() {
+        v_MaxIter = 30;
+        v_MinRe = - 2.0;
+        v_MaxRe = 1.0;
+        v_MinIm = - 1.2;
+        v_MaxIm = v_MinIm + (v_MaxRe - v_MinRe) * getHeight() / getWidth();
+        v_Re_scale = (v_MaxRe - v_MinRe) / (getWidth() - 1);
+        v_Im_scale = (v_MaxIm - v_MinIm) / (getHeight() - 1);
+        _imageScopeVarsInitialized = true;
+    }
+
+    public void evaluate(double _x, double _y) {
+        if (!isWorldSet()) {
+            setDefaultBounds();
+        }
+        if (!_imageScopeVarsInitialized) {
+            initImageScopeVars();
+        }
+        _stk.clear();
+
+        double v_c_im = v_MaxIm - _y * v_Im_scale;
+        double v_c_re = v_MinRe + _x * v_Re_scale;
+        double v_Z_re = v_c_re;
+        double v_Z_im = v_c_im;
+        double v_outside = 0;
+        double v_n = 0;
+        while (_FN.sign(_FN.GE(v_n, v_MaxIter)) == 0) {
+            double v_Z_re2 = v_Z_re * v_Z_re;
+            double v_Z_im2 = v_Z_im * v_Z_im;
+            v_outside = _FN.GT(v_Z_re2 + v_Z_im2, 4);
+            if (_FN.sign(v_outside) == 1) break;
+            v_Z_im = 2 * v_Z_re * v_Z_im + v_c_im;
+            v_Z_re = v_Z_re2 - v_Z_im2 + v_c_re;
+            v_n++;
+        }
+        writeToImage("result", _x, _y, 0, v_outside);
+    }
+}
